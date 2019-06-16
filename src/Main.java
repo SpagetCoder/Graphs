4cd8e92d783d8dcf32ef4ddbfcd1ba173fcb4602
SpagetCoder <quassa.mody@gmail,com>
2019-06-16 13:04:54 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main
 {
-    final static int INF = 9999;
-
     public static void main (String[] args)
     {
         int graph[][] = {{1, 2 ,3}, {1, 3 ,8}, {1, 5 ,-4}, {2, 4 ,1}, {2, 5 ,7}, {3, 2 ,4}, {4, 1 ,2}, {4, 3 ,-5}, {5, 4 ,6}};
